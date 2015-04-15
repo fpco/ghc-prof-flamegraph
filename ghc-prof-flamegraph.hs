626ec87a6c39ee416de2d012e7c8b804e5df0379
Francesco Mazzoli <f@mazzo.li>
2015-04-15 11:52:09 +0200

    Remove warnings.

--- ghc-prof-flamegraph.hs
+++ ghc-prof-flamegraph.hs
@@ -1,6 +1,5 @@
-import           Data.List (intercalate, isPrefixOf)
+import           Data.List (intercalate)
 import           Data.Char (isSpace)
-import           Debug.Trace
 
 parseLine :: String -> (String, String, String)
 parseLine s = case words s of
@@ -14,21 +13,22 @@ processLines = go [] []
   where
     go :: [String] -> [String] -> [String] -> [String]
     go _stack frames [] = reverse frames
-    go stack0 frames (line : lines) = 
+    go stack0 frames (line : lines') =
       let (spaces, rest) = break (not . isSpace) line
           stack = take (length spaces) stack0
           (costCentre, module_, entries) = parseLine rest
           symbol = module_ ++ "." ++ costCentre
           stack' = symbol : stack
           frame = intercalate ";" (reverse stack') ++ " " ++ entries
-      in go stack' (frame : frames) lines
+      in go stack' (frame : frames) lines'
+
+firstLine :: [String]
+firstLine = ["COST", "CENTRE", "MODULE", "no.", "entries", "%time", "%alloc", "%time", "%alloc"]
 
 findStart :: [String] -> [String]
 findStart [] = error "findStart: malformed .prof file"
-findStart (line1 : line2 : lines) | words line1 == firstLine = lines
-  where
-    firstLine = ["COST", "CENTRE", "MODULE", "no.", "entries", "%time", "%alloc", "%time", "%alloc"]
-findStart (line : lines) = findStart lines
+findStart (line : _empty : lines') | words line == firstLine = lines'
+findStart (_line : lines') = findStart lines'
 
 main :: IO ()
 main = do
