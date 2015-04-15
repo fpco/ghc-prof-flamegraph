669edce50fb4a815a4a2dc2eb3335b5da8ff8ee5
Francesco Mazzoli <f@mazzo.li>
2015-04-15 11:50:21 +0200

    Fix bug in finding the start of the table.
    Wasn't the last commit after all!

--- ghc-prof-flamegraph.hs
+++ ghc-prof-flamegraph.hs
@@ -1,5 +1,6 @@
-import           Data.List (intercalate)
+import           Data.List (intercalate, isPrefixOf)
 import           Data.Char (isSpace)
+import           Debug.Trace
 
 parseLine :: String -> (String, String, String)
 parseLine s = case words s of
@@ -13,7 +14,7 @@ processLines = go [] []
   where
     go :: [String] -> [String] -> [String] -> [String]
     go _stack frames [] = reverse frames
-    go stack0 frames (line : lines) =
+    go stack0 frames (line : lines) = 
       let (spaces, rest) = break (not . isSpace) line
           stack = take (length spaces) stack0
           (costCentre, module_, entries) = parseLine rest
@@ -22,8 +23,14 @@ processLines = go [] []
           frame = intercalate ";" (reverse stack') ++ " " ++ entries
       in go stack' (frame : frames) lines
 
+findStart :: [String] -> [String]
+findStart [] = error "findStart: malformed .prof file"
+findStart (line1 : line2 : lines) | words line1 == firstLine = lines
+  where
+    firstLine = ["COST", "CENTRE", "MODULE", "no.", "entries", "%time", "%alloc", "%time", "%alloc"]
+findStart (line : lines) = findStart lines
+
 main :: IO ()
 main = do
   s <- getContents
-  -- 27 is the number of lines before the actual data starts.
-  putStr $ unlines $ processLines $ drop 27 $ lines s
+  putStr $ unlines $ processLines $ findStart $ lines s
