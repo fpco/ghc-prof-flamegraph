2cde7a2a43536243baeaf111fa23239799a523e8
Alexander Vershilov <alexander.vershilov@tweag.io>
2016-11-09 23:11:40 +0300

    Implement more counters modes.
    New counters were implemented:
      - bytes - calculate amount of bytes allocated by each function.
      - ticks - calculate number of ticks spent in each function.
    
    Main rationale is that alloc and time in percentage is fairily
    unusable in non-trivial programs or programs that spends much time
    in idle. So both bytes and ticks gives better stats.
    
    Additional modes require program to be run with -P, so additional
    checks were introduced in order to notify user about this requirement.

--- ghc-prof-flamegraph.hs
+++ ghc-prof-flamegraph.hs
@@ -1,43 +1,84 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
-import           Control.Applicative ((<*>))
+import           Control.Applicative ((<*>), (<|>))
 import           Data.Functor ((<$>))
 import           Data.List (intercalate)
 import           Data.Monoid ((<>))
 import qualified Options.Applicative as Opts
+import           System.Exit (exitFailure)
+import           System.IO (stderr, hPutStrLn)
 import qualified ProfFile as Prof
 
 data Options = Options
-  { optionsAlloc :: Bool
+  { optionsReportType :: ReportType
   } deriving (Eq, Show)
+
+
+data ReportType = Alloc   -- ^ Report allocations, percent
+                | Entries -- ^ Report entries, number
+                | Time    -- ^ Report time spent in closure, percent
+                | Ticks   -- ^ Report ticks, number
+                | Bytes   -- ^ Report bytes allocated, number
+                deriving (Eq, Show)
 
 optionsParser :: Opts.Parser Options
 optionsParser = Options
-  <$> Opts.switch (Opts.long "alloc" <> Opts.help "Uses the allocation measurements instead of time measurements")
+  <$> (Opts.flag' Alloc (Opts.long "alloc" <> Opts.help "Uses the allocation measurements instead of time measurements")
+       <|> Opts.flag' Entries (Opts.long "entry" <> Opts.help "Uses entries the measurements instead of time measurements")
+       <|> Opts.flag' Bytes   (Opts.long "bytes" <> Opts.help "Memory measurements in bytes (+RTS -P -RTS)")
+       <|> Opts.flag' Ticks (Opts.long "ticks" <> Opts.help "Time measurements in ticks (+RTS -P -RTS)")
+       <|> Opts.flag  Time Time (Opts.long "time" <> Opts.help "Uses time measurements"))
+
+checkNames :: ReportType -> [String] -> Maybe String
+checkNames Alloc   _ = Nothing
+checkNames Entries _ = Nothing
+checkNames Time    _ = Nothing
+checkNames Ticks   n
+  | "ticks" `elem` n = Nothing
+  | otherwise        = Just "No ticks information, please run program with +RTS -P"
+checkNames Bytes n
+  | "bytes" `elem` n = Nothing
+  | otherwise        = Just "No ticks information, please run program with +RTS -P"
+
+normalize :: ReportType -> Double -> Int
+normalize Alloc = round . (10 *)
+normalize Time  = round . (10 *)
+normalize _     = round
+
+addUnknown :: ReportType -> (Int, [String]) -> [String]
+addUnknown Time   = \(entries, frames) ->
+  let unknown = 1000 - entries
+  in if unknown > 0
+     then ("UNKNOWN " ++ show unknown) : frames
+     else frames
+addUnknown Alloc   = \(entries, frames) ->
+  let unknown = 1000 - entries
+  in if unknown > 0
+     then ("UNKNOWN " ++ show unknown) : frames
+     else frames
+addUnknown _ = snd
 
 generateFrames :: Options -> [Prof.Line] -> [String]
-generateFrames options lines0 =
-  let (entries, frames) = go [] lines0
-      unknown = 1000 - entries
-  in if unknown > 0
-    then ("UNKNOWN " ++ show unknown) : frames
-    else frames
+generateFrames options lines0 = addUnknown (optionsReportType options) $ go [] lines0
   where
     go :: [String] -> [Prof.Line] -> (Int, [String])
     go _stack [] =
       (0, [])
     go stack (line : lines') =
-      let entries :: Int = round $ 10 * (individualMeasure line)
+      let entries = normalize (optionsReportType options) (individualMeasure line)
           symbol = Prof.lModule line ++ "." ++ Prof.lCostCentre line
           frame = intercalate ";" (reverse (symbol : stack)) ++ " " ++ show entries
           (childrenEntries, childrenFrames) = go (symbol : stack) (Prof.lChildren line)
           (restEntries, restFrames) = go stack lines'
       in (entries + childrenEntries + restEntries, frame : childrenFrames ++ restFrames)
 
-    individualMeasure = if optionsAlloc options
-      then Prof.lIndividualAlloc
-      else fromIntegral . Prof.lEntries
+    individualMeasure = case optionsReportType options of
+      Alloc   -> Prof.lIndividualAlloc
+      Time    -> Prof.lIndividualTime
+      Entries -> fromIntegral . Prof.lEntries
+      Ticks   -> fromIntegral . Prof.lTicks
+      Bytes   -> fromIntegral . Prof.lBytes
 
 main :: IO ()
 main = do
@@ -46,4 +87,8 @@ main = do
   s <- getContents
   case Prof.parse s of
     Left err -> error err
-    Right ls -> putStr $ unlines $ generateFrames options ls
+    Right (names, ls) ->
+      case checkNames (optionsReportType options) names of
+        Just problem -> do hPutStrLn stderr problem
+                           exitFailure
+        Nothing ->  putStr $ unlines $ generateFrames options ls
